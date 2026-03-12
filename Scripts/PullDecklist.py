8521e60b74ab39e88eb962d29ea4b2dbdc0c3175
William Möller <91247643+SkogsTomte04@users.noreply.github.com>
2026-03-13 11:24:21 +1300

    Added scryfall dataset
    also added a script to get all TLA cards from the dataset

--- Scripts/PullDecklist.py
+++ Scripts/PullDecklist.py
@@ -1,31 +1,64 @@
 import requests,json
 
 
+data = []
+cards = {'R': [],
+         'G': [],
+         'W' : [],
+         'B': [],
+         'U': [],
+         'N': [],
+         'M': []}
+with open('Scripts\scryfall\scryfalldataset.json','r', encoding='utf-8') as file:
+    data = json.load(file)
 
-header = {
-    'User-Agent': 'MagicReach/1.0',
-    'Accept': '*/*'
-}
-payload = {
-    'identifiers': [
-        {
-            'id': '5e51f727-5a9b-4bc7-83a9-dbcf1c933e15'
-        },
-        {
-            'name': "Aang's Journey"
-        },
-        {
-            'set': 'tla',
-            'collector_number': '1'
-        }
+def SortCard(card):
+    color = card['color_identity']
+    if len(color) > 1:
+        cards['M'].append(card)
+        return
+    if len(color) < 1:
+        cards['N'].append(card)
+        return
 
-    ]
-}
-# x = requests.get('https://api.scryfall.com/cards/named?fuzzy=aust+com', headers=header)
-x = requests.post('https://api.scryfall.com/cards/collection', headers=header,json=payload)
+    cards[color[0]].append(card)
+    
+for card in data:
+    if card['set'] == 'tla':
+        #print(card['name'], card['color_identity'],)
+        SortCard(card)
 
-data = json.loads(x.text)
-keys = data['data']
-for item in data['data']:
-    print(item['name'])
+
+
+for card in cards['R']:
+    print(card['name'])
+#print(data[0]['set'])
+
+# header = {
+#     'User-Agent': 'MagicReach/1.0',
+#     'Accept': '*/*'
+# }
+# payload = {
+#     'identifiers': [
+#         {
+#             'id': '5e51f727-5a9b-4bc7-83a9-dbcf1c933e15'
+#         },
+#         {
+#             'name': "Aang's Journey"
+#         },
+#         {
+#             'set': 'tla',
+#             'collector_number': '1'
+#         }
+
+#     ]
+# }
+# x = requests.get('https://api.scryfall.com/sets/tla', headers=header)
+# # x = requests.post('https://api.scryfall.com/cards/collection', headers=header,json=payload)
+
+# data = json.loads(x.text)
+# keys = data.keys()
+# print(keys)
+# for item in data['data']:
+#     print(item['name'])
    
